3d79733a8f83e77e5c4ddce8f47ff13ebbdd55f9
Paul Sroczynski <p.sroczynski@gmail.com>
2021-10-17 13:00:11 +0200

    Initial solution

---
+++ src/DatabaseFixture/DatabaseFixture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseFixture
+{
+    public class DatabaseFixture
+    {
+        public DatabaseFixture()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
