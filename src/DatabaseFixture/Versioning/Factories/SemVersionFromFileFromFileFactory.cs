ddbd723ac2ad4d2c31b1e7c3c47c37081e6b7278
Paul Sroczynski <p.sroczynski@gmail.com>
2021-10-18 16:03:14 +0200

    Filename to SemVer parser

--- src/DatabaseFixture/Versioning/Factories/SemVersionFromFileFromFileFactory.cs
+++ src/DatabaseFixture/Versioning/Factories/SemVersionFromFileFromFileFactory.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 using Ardalis.GuardClauses;
 
 namespace DatabaseFixture.Versioning.Factories
@@ -8,8 +10,32 @@ namespace DatabaseFixture.Versioning.Factories
         public IVersion Create(FileInfo file)
         {
             Guard.Against.FileExists(file, nameof(file));
-            // ToDo: parse filename, to create semver format
-            return new SemVerVersion(1, 0, 0);
+
+            var versionParts = Parse(file.Name);
+
+            return new SemVerVersion(
+                versionParts.Major,
+                versionParts.Minor, 
+                versionParts.Patch
+            );
+        }
+
+        private static dynamic Parse(string filename)
+        {
+            var matches = Regex.Match(filename, 
+                @"^(?<Major>[0-9]+)\.(?<Minor>[0-9]+)\.(?<Patch>[0-9]+).*", 
+                RegexOptions.IgnoreCase | RegexOptions.Singleline);
+
+            if (matches.Success == false)
+            {
+                throw new NotSupportedException(@$"Filename ""{filename}"" is not supported with SemVer versioning strategy");
+            }
+
+            var major = int.Parse(matches.Groups["Major"].Value);
+            var minor = int.Parse(matches.Groups["Minor"].Value);
+            var patch = int.Parse(matches.Groups["Patch"].Value);
+
+            return new { Major = major, Minor = minor, Patch = patch };
         }
     }
 }
